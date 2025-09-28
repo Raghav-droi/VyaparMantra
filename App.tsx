e9363935af56832f2db154d0f09fa29cfb9f7725
Macbook <macbook@Macbooks-MacBook-Air.local>
2025-09-28 16:54:06 +0530

    Login_Page_Changes

--- App.tsx
+++ App.tsx
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import SplashScreen from './SplashScreen';
 import Home_Page_Login from './Home_Page_Login';
+import { TruckElectric } from 'lucide-react';
 
 const MainApp = () => {
-  return (
-   <Home_Page_Login/>
-  )
+  return <Home_Page_Login />;
 };
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  
 
   useEffect(() => {
     const timer = setTimeout(() => setLoading(false), 3000); // Show splash 3 seconds
